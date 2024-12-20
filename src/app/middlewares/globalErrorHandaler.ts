2a4ff9fbdc36ad8a54441c34ef6c97047dce8184
codermahbub20 <mkrefat5@gmail.com>
2024-12-21 02:21:03 +0600

    Global error handaler created

---
+++ src/app/middlewares/globalErrorHandaler.ts
@@ -0,0 +1,84 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ErrorRequestHandler } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../interface/error';
+import config from '../config';
+import handleZodError from '../Errors/handaleZodError';
+import handleValidationError from '../Errors/handaleValidationError';
+import handleCastError from '../Errors/handaleCastError';
+import AppError from '../Errors/AppError';
+import handleCDuplicateError from '../Errors/handleCDuplicateError';
+
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next): any => {
+  let statusCode = 500;
+  let message = 'Something went wrong!';
+
+  let errorSources: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong!',
+    },
+  ];
+
+  // Create a middleware
+
+  if (err instanceof ZodError) {
+    //zod error here
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'ValidationError') {
+    // Validation error Here
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    // Cast Error Here
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.code === 11000) {
+    //  Manage 1000 duplicate error
+    const simplifiedError = handleCDuplicateError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err instanceof AppError) {
+    //  Manage app error
+
+    statusCode = err?.statusCode;
+    message = err?.message;
+    errorSources = [
+      {
+        path: '',
+        message: err.message,
+      },
+    ];
+  } else if (err instanceof Error) {
+    //  Manage app error
+
+    message = err?.message;
+    errorSources = [
+      {
+        path: '',
+        message: err.message,
+      },
+    ];
+  }
+
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    // error: err,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
+  });
+};
+
+export default globalErrorHandler;
