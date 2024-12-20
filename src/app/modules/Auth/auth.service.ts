92de37223608a5370b5afca2ed1af4b3bb7d9559
codermahbub20 <mkrefat5@gmail.com>
2024-12-20 23:38:42 +0600

    Auth login service created

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,10 +1,51 @@
 import { User } from '../User/user.model';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const register = async (payload: string) => {
   const result = await User.create(payload);
   return result;
 };
 
+const login = async (payload: { email: string; password: string }) => {
+  // checking if the user is exist
+  const user = await User.findOne({ email: payload?.email }).select(
+    '+password',
+  );
+
+  if (!user) {
+    throw new Error('This user is not found !');
+  }
+
+  // checking if the user is inactive
+  const userStatus = user?.isBlocked;
+
+  if (userStatus === true) {
+    throw new Error('This user is blocked ! !');
+  }
+
+  //checking if the password is correct
+  const isPasswordMatched = await bcrypt.compare(
+    payload?.password,
+    user?.password,
+  );
+
+  if (!isPasswordMatched) {
+    throw new Error('Wrong Password!!! Tell me who are you? 😈');
+  }
+
+  //create token and sent to the  client
+  const jwtPayload = {
+    email: user?.email,
+    role: user?.role,
+  };
+
+  const token = jwt.sign(jwtPayload, 'secret', { expiresIn: '1d' });
+
+  return { token, user };
+};
+
 export const AuthService = {
   register,
+  login,
 };
